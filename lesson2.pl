aff3b04b310c4053d4a92d028c1e5a86828391b7
Daniel Siegle <q837851@usadc-seaxp58>
2017-03-27 13:53:37 -0400

    added UNC course exercise

---
+++ lesson2.pl
@@ -0,0 +1,20 @@
+#!usr/bin/perl
+# Write an if-statement that executes only if the value in $a is numerically greater than $b
+
+if ($a > $b) { };
+
+if($a lt $b) {
+      print "hello\n";
+    } else {
+      print "bye\n";
+    }
+
+ 
+chomp($input = <STDIN>);
+$answer = "Hello there,";
+if ($input eq $answer) {
+   print "XXX";
+   } else {
+   print "YYY";
+ }
+

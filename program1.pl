d3b2ca9ed468b2b277f4ede0d8ab9c99b39d701c
Daniel Siegle <q837851@usadc-seaxp58>
2017-04-13 16:26:00 -0400

    assignment 1

--- program1.pl
+++ program1.pl
@@ -1,3 +1,14 @@
+#!/usr/bin/perl
+
+
+print "Please enter a good name or --DONE--:";
+chomp($name_input = <STDIN>);
+
+print "What is this person's area code?:";
+chomp($area_code_input = <STDIN>);
+
+print "What is this person's phone number?:";
+chomp($phone_input = <STDIN>);
 
 
 Ask for a name - full name entered followed by a return
@@ -31,6 +42,6 @@ Work Life		(313) 555-7777
 6. You must have a header and some lines underneath as in my example (dashes work well).
 7. You must print all information from one person on a single line - don't put the name on one line and the phone number on another.
 8. Make the output look nice. The spacing between questions and so on doesn't have to look exactly like mine, but running lines together, for instance, would make you lose some points.
-9. The on;y comment I need is your name at the top, but it's always a good idea to comment your code as a reference for the future.
+9. The only comment I need is your name at the top, but it's always a good idea to comment your code as a reference for the future.
 10. Do not use anything covered in Chapter 1-3; for example, don't use the length or printf functions.
 11. You connot use any modules in this program.
